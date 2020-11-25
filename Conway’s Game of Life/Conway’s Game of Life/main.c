0fba4dfd18f55633ba759b0fe31845c7a31260de
simon.s <simon2204s@gmail.com>
2020-11-25 01:30:19 +0100

    Verbesserungen

--- Conway’s Game of Life/Conway’s Game of Life/main.c
+++ Conway’s Game of Life/Conway’s Game of Life/main.c
@@ -10,49 +10,58 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+enum neighbour: int {
+    topLeft, topRight,
+    bottomLeft, bottomRight,
+    top, bottom,
+    left, right,
+    all_neighbours
+};
+
+// MARK: Required functions
+
 void print_generation(void);
 void set_generation_from_string(char string[]);
 void get_generation_as_string(char string[]);
 bool set_next_generation(void);
 void game_of_life(int max_generations);
 
+// MARK: Custom functions
+
 char char_in_field(short row, short column);
-bool is_set(unsigned char generation[], short pos);
-bool is_set_for_field(unsigned char generation[], short row, short column);
-void set_bit(unsigned char generation[], short pos);
+bool is_set(short index, unsigned char generation[]);
+bool is_set_for_cell(short row, short column, unsigned char generation[]);
+bool is_set_for_neighbour(enum neighbour n, short center_index, unsigned char generation[]);
+void set_bit(short index, unsigned char generation[]);
 void set_bit_for_field(short row, short column);
-void delete_bit(unsigned char generation[], short pos);
+void delete_bit(short index, unsigned char generation[]);
 void delete_bit_for_field(short row, short column);
-short neighbours_count(unsigned char generation[], short pos);
+short count_neighbours(short index, unsigned char generation[]);
 
 #define ALL_ROWS 10
 #define ALL_COLS 10
-#define ALL_FIELDS (ALL_ROWS * ALL_COLS)
-#define ALL_FIELDS_IN_BYTE (ALL_FIELDS / 8)
-#define SEGMENTS (ALL_FIELDS_IN_BYTE + 1)
-
-#define UP -1
-#define DOWN 1
-#define LEFT -1
-#define RIGHT 1
-#define ZERO 0
+#define ALL_CELLS (ALL_ROWS * ALL_COLS)
+#define ALL_CELLS_IN_BYTE (ALL_CELLS / 8)
+#define SEGMENTS (ALL_CELLS_IN_BYTE + 1)
 
 unsigned char generation[SEGMENTS];
 unsigned char generation_copy[SEGMENTS];
 
+// MARK: MAIN
+
 int main(int argc, const char * argv[]) {
     
-    char glider[] =
-    "0000000100"
-    "0000000101"
-    "0010000110"
-    "0010100000"
-    "0011000000"
-    "0000000000"
-    "0000000000"
-    "0000000000"
-    "0000000000"
-    "0000000000";
+//    char glider[] =
+//    "0000000100"
+//    "0000000101"
+//    "0010000110"
+//    "0010100000"
+//    "0011000000"
+//    "0000000000"
+//    "0000000000"
+//    "0000000000"
+//    "0000000000"
+//    "0000000000";
     
     char sonne[] =
     "0001100000"
@@ -72,6 +81,8 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+// MARK: Initialisierung
 
 void game_of_life(int max_generations)
 {
@@ -99,15 +110,15 @@ bool set_next_generation()
         generation_copy[j] = generation[j];
     }
     
-    for (i = 0; i < ALL_FIELDS; i++)
+    for (i = 0; i < ALL_CELLS; i++)
     {
-        n_count = neighbours_count(generation_copy, i);
+        n_count = count_neighbours(i, generation_copy);
         
-        if (is_set(generation_copy, i))
+        if (is_set(i, generation_copy))
         {
             if (n_count < 2 || n_count > 3)
             {
-                delete_bit(generation, i);
+                delete_bit(i, generation);
                 did_mutate = true;
             }
         }
@@ -115,7 +126,7 @@ bool set_next_generation()
         {
             if (n_count == 3)
             {
-                set_bit(generation, i);
+                set_bit(i, generation);
                 did_mutate = true;
             }
         }
@@ -124,18 +135,18 @@ bool set_next_generation()
     return did_mutate;
 }
 
-void set_generation_from_string(char string[ALL_FIELDS])
+void set_generation_from_string(char string[ALL_CELLS])
 {
     int i = 0;
     char charakter = string[i];
     while (charakter != '\0')
     {
         if (charakter == '0') {
-            delete_bit(generation, i);
+            delete_bit(i, generation);
         }
         else
         {
-            set_bit(generation, i);
+            set_bit(i, generation);
         }
         
         i++;
@@ -143,81 +154,106 @@ void set_generation_from_string(char string[ALL_FIELDS])
     }
 }
 
-void get_generation_as_string(char string[ALL_FIELDS]) {
+void get_generation_as_string(char string[ALL_CELLS])
+{
     int i;
-    for (i = 0; i < ALL_FIELDS; i++)
+    for (i = 0; i < ALL_CELLS; i++)
     {
-        printf("%c", is_set(generation, i) ? '1' : '0');
+        string[i] = is_set(i, generation) ? '1' : '0';
     }
-    printf("\n");
 }
 
-void delete_bit(unsigned char generation[SEGMENTS], short pos)
+// MARK: Set and delete Bit
+
+void delete_bit(short index, unsigned char generation[SEGMENTS])
 {
-    if (pos < 0 || pos >= ALL_FIELDS)
+    if (index < 0 || index >= ALL_CELLS)
     {
         return;
     }
-    short segment = pos / 8;
-    short element = pos % 8;
+    short segment = index / 8;
+    short element = index % 8;
     generation[segment] &= ~(0x80u >> element);
 }
 
-void set_bit(unsigned char generation[SEGMENTS], short pos)
+void set_bit(short index, unsigned char generation[SEGMENTS])
 {
-    if (pos < 0 || pos >= ALL_FIELDS)
+    if (index < 0 || index >= ALL_CELLS)
     {
         return;
     }
-    short segment = pos / 8;
-    short element = pos % 8;
+    short segment = index / 8;
+    short element = index % 8;
     generation[segment] |= (0x80u >> element);
 }
 
-bool is_set(unsigned char generation[SEGMENTS], short pos)
+// MARK: Is bit set?
+
+bool is_set(short index, unsigned char generation[SEGMENTS])
 {
-    if (pos < 0 || pos >= ALL_FIELDS)
+    if (index < 0 || index >= ALL_CELLS)
     {
         return false;
     }
-    short segment = pos / 8;
-    short element = pos % 8;
+    short segment = index / 8;
+    short element = index % 8;
     return ((0x80u >> element) & generation[segment]) > 0;
 }
 
-bool is_set_for_field(unsigned char generation[SEGMENTS], short row, short column)
+bool is_set_for_cell(short row, short column, unsigned char generation[SEGMENTS])
 {
     if (row < 0 || column < 0 || row >= ALL_ROWS || column >= ALL_COLS)
     {
         return false;
     }
-    return is_set(generation, (row * ALL_COLS) + column);
+    return is_set((row * ALL_COLS) + column, generation);
 }
 
-short neighbours_count(unsigned char generation[SEGMENTS], short pos)
+bool is_set_for_neighbour(enum neighbour n, short center_index, unsigned char generation[])
 {
-    short row_directions[]    = {   UP, ZERO,   UP,   UP,  DOWN,  ZERO, DOWN,  DOWN };
-    short column_directions[] = { ZERO, LEFT, LEFT, RIGHT, ZERO, RIGHT, LEFT, RIGHT };
-    short own_row = pos / (ALL_COLS);
-    short own_column = pos % (ALL_COLS);
-    short count = 0;
-    int i;
+    short row = center_index / ALL_COLS;
+    short column = center_index % ALL_COLS;
+    switch (n) {
+        case topLeft:
+            return is_set_for_cell(row - 1, column - 1, generation);
+        case topRight:
+            return is_set_for_cell(row - 1, column + 1, generation);
+        case bottomLeft:
+            return is_set_for_cell(row + 1, column - 1, generation);
+        case bottomRight:
+            return is_set_for_cell(row + 1, column + 1, generation);
+        case top:
+            return is_set_for_cell(row - 1, column + 0, generation);
+        case bottom:
+            return is_set_for_cell(row + 1, column + 0, generation);
+        case left:
+            return is_set_for_cell(row + 0, column - 1, generation);
+        case right:
+            return is_set_for_cell(row + 0, column + 1, generation);
+        default:
+            return false;
+    }
+}
+
+// MARK: Count neighbours
+
+short count_neighbours(short center_index, unsigned char generation[SEGMENTS])
+{
+    short n_count = 0;
+    enum neighbour n;
     
-    for (i = 0; i < 9; i++)
+    for (n = 0; n < all_neighbours; n++)
     {
-        if (is_set_for_field(generation, own_row + row_directions[i], own_column + column_directions[i]))
+        if (is_set_for_neighbour(n, center_index, generation))
         {
-            count++;
+            n_count++;
         }
     }
     
-    return count;
+    return n_count;
 }
 
-char char_in_field(short row, short column)
-{
-    return is_set_for_field(generation, row, column) ? 'o' : ' ';
-}
+// MARK: Print Generation
 
 void print_generation()
 {
@@ -247,4 +283,9 @@ void print_generation()
         }
         printf("+\n");
     }
+}
+
+char char_in_field(short row, short column)
+{
+    return is_set_for_cell(row, column, generation) ? 'o' : ' ';
 }
