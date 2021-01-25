b3e7baf9c1d1a7c17c86e417a05ed8d4c3aae93f
simon.s <simon2204s@gmail.com>
2021-01-25 03:16:01 +0100

    Verbesserungen

--- SwiftHeap/Heap/Heap/Heap.swift
+++ SwiftHeap/Heap/Heap/Heap.swift
@@ -4,66 +4,86 @@
 //
 //  Created by Simon Schöpke on 24.01.21.
 //
+import Foundation
 
 struct Heap<Element: Comparable> {
-    private var elements = ContiguousArray<Element>()
-
-    mutating func insert(_ newElement: Element) {
-        elements.append(newElement)
-        swim(startIndex: elements.count - 1)
+    private var elements: UnsafeMutableBufferPointer<UnsafeMutablePointer<Element>>
+    private(set) var count = 0
+    private(set) var capacity: Int
+    
+    init(capacity: Int = 10000) {
+        self.capacity = capacity
+        elements = UnsafeMutableBufferPointer<UnsafeMutablePointer<Element>>.allocate(capacity: capacity)
     }
 
-    mutating func extractMin() -> Element? {
-        defer {
-            if let last = elements.last {
-                elements[0] = last
-                elements.removeLast()
-                sink(startIndex: 0)
-            }
-        }
+    mutating func insert(_ newElement: UnsafeMutablePointer<Element>) {
+        elements[count] = newElement
+        swim(startIndex: count)
+        count += 1
+    }
 
-        return elements.first
+    private mutating func extractMin() -> UnsafeMutablePointer<Element>? {
+        guard count > 0 else { return nil }
+        defer {
+            count -= 1
+            elements[0] = elements[count]
+            sink(startIndex: 0)
+        }
+        return elements[0]
     }
     
     private mutating func swim(startIndex: Int) {
         if startIndex == 0 { return }
-        let parentIdx = parentIndex(forChildIndex: startIndex)
-        if elements[startIndex] < elements[parentIdx] {
+        let parentIdx = (startIndex - 1) / 2
+        if elements[startIndex].pointee < elements[parentIdx].pointee {
             elements.swapAt(startIndex, parentIdx)
             swim(startIndex: parentIdx)
         }
     }
     
     private mutating func sink(startIndex: Int) {
-        guard !elements.isEmpty else { return }
-        let parent = elements[startIndex]
-        let leftChildIdx = leftChildIndex(forParentIndex: startIndex)
-        let rightChildIdx = rightChildIndex(forParentIndex: startIndex)
-        if rightChildIdx < elements.count {
-            let leftChild = elements[leftChildIdx]
-            let rightChild = elements[rightChildIdx]
-            if leftChild < rightChild && leftChild < parent {
+        let leftChildIdx = 2 * startIndex + 1
+        let rightChildIdx = leftChildIdx + 1
+        if rightChildIdx < count {
+            let leftChild = elements[leftChildIdx].pointee
+            let rightChild = elements[rightChildIdx].pointee
+            if leftChild < rightChild && leftChild < elements[startIndex].pointee {
                 elements.swapAt(leftChildIdx, startIndex)
                 sink(startIndex: leftChildIdx)
-            } else if rightChild < parent {
+            } else if rightChild < elements[startIndex].pointee {
                 elements.swapAt(rightChildIdx, startIndex)
                 sink(startIndex: rightChildIdx)
             }
-        } else if leftChildIdx < elements.count && elements[leftChildIdx] < parent {
+        } else if leftChildIdx < count
+                    && elements[leftChildIdx].pointee < elements[startIndex].pointee {
             elements.swapAt(leftChildIdx, startIndex)
             sink(startIndex: leftChildIdx)
         }
     }
     
-    private func leftChildIndex(forParentIndex parentIndex: Int) -> Int {
-        return 2 * parentIndex + 1
+    func `deinit`() {
+        elements.deallocate()
     }
-    
-    private func rightChildIndex(forParentIndex parentIndex: Int) -> Int {
-        return 2 * parentIndex + 2
+}
+
+
+extension Heap: Sequence {
+    func makeIterator() -> HeapIterator {
+        return HeapIterator(self)
     }
-    
-    private func parentIndex(forChildIndex childIndex: Int) -> Int {
-        return (childIndex - 1) / 2
+}
+
+
+extension Heap {
+    struct HeapIterator: IteratorProtocol {
+        private var heap: Heap
+        
+        init(_ heap: Heap) {
+            self.heap = heap
+        }
+        
+        mutating func next() -> UnsafeMutablePointer<Element>? {
+            return heap.extractMin()
+        }
     }
 }
